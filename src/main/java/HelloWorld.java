3bf9b6391e48dcdfdeebe4d418264a40418970ef
dearyxin <dearyxin@sina.com>
2016-03-08 18:58:48 +0800

    my first java project in github

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yxin-PC on 2016/3/8.
+ */
+public class HelloWorld {
+   public static void main(String args[]){
+       System.out.print("my first java project in the github");
+   }
+}
