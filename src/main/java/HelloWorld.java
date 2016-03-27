1be6e84e9bfa8db7f8b345dc7769267239c6ea1f
dearyxin <dearyxin@sina.com>
2016-03-27 23:53:13 +0800

    this is mt test for reset

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,11 @@
+import java.lang.Exception;
+
 /**
  * Created by yxin-PC on 2016/3/8.
  */
-public class HelloWorld {
-   public static void main(String args[]){
-       System.out.print("my first java project in the github");
+public class HelloWorld{
+   public static void main(String args[]) throws Exception {
+       System.out.print("my first java project in the github     fail");
+       System.out.print("");
    }
 }
