956462ad0e4b07a2ce74ea810c003e7699455647
dearyxin <dearyxin@sina.com>
2016-03-27 23:08:36 +0800

    hhh

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,10 @@
+import java.lang.Exception;
+
 /**
  * Created by yxin-PC on 2016/3/8.
  */
-public class HelloWorld {
-   public static void main(String args[]){
+public class HelloWorld{
+   public static void main(String args[]) throws Exception {
        System.out.print("my first java project in the github");
    }
 }
